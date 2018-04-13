987675caf354c88de3c31e9f452d93fb5c928424
ahorn80 <ahorn80@gmx.de>
2018-04-13 20:07:39 +0200

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,16 @@
 package ru.job4j;
 
-
+/**
+*Class Calculate for calculation
+*@author Andreas Horn
+*@since 13.04.18
+*/
 public class Calculate{
 
+	/**
+	*Main method
+	*@param args list of arguments
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task
+*
+*@author Andreas Horn(ahorn80@yandex.ru)
+*@version 1.0
+*/
+
+package ru.job4j;
+
