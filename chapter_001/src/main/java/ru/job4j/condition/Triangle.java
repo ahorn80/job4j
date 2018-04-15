aa4eaf1217daba793f785b871822006fdd77ea6d
ahorn80 <ahorn80@gmx.de>
2018-04-15 14:00:34 +0200

    3.3. Вычисление площади треугольника

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,69 @@
+package ru.job4j.condition;
+
+/**
+ * @author Andreas Horn (ahorn80@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    /**
+     * Triangle from 3 points
+     * @param a point a
+     * @param b point b
+     * @param c point c
+     */
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисления полупериметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (ab + ac + bc) / 2
+     *
+     * @param ab расстояние между точками a b
+     * @param ac расстояние между точками a c
+     * @param bc расстояние между точками b c
+     * @return Полуперимент.
+     */
+    public double halfPerimeter(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * @return Вернуть прощадь, если треугольник существует или -1, если треугольника нет.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.halfPerimeter(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return (ab > 0 && ac > 0 && bc > 0);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // создаем три объекта класса Point.
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+        // Создаем объект треугольник и передаем в него объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // Вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    @Test
+    public void whenEnterInvalidTriangleThenInvalidArea() {
+        Point a = new Point(1, 1);
+        Triangle triangle = new Triangle(a, a, a);
+        double result = triangle.area();
+        double expected = -1D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    @Test
+    public void whenEnterThreePointsOnSameLineThenAreaNull() {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+        Point c = new Point(3, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 0D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
