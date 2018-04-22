7e97af3c67e53a582338077dabb246c97d95ecb6
ahorn80 <ahorn80@gmx.de>
2018-04-22 11:35:10 +0200

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -0,0 +1,10 @@
+package ru.job4j.professions;
+
+public class Profession {
+    private String name;
+    private String occupation;
+
+    public String getName() {
+        return this.name;
+    }
+}
