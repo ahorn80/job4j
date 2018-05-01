0f7d77dd42b1d0630d38fa33d7ca258f7821b22f
ahorn80 <ahorn80@gmx.de>
2018-05-01 13:38:27 +0200

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.pseudo;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 import java.io.ByteArrayOutputStream;
@@ -8,12 +10,27 @@ import java.io.PrintStream;
 
 public class PaintTest {
     final java.lang.String lineSeparator = System.getProperty("line.separator");
+    /**
+     * default output to console
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * buffer for output
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(out.toString(), is(
                 new StringBuilder()
@@ -24,24 +41,19 @@ public class PaintTest {
                 .append("o o o o o").append(lineSeparator).append(lineSeparator)
                 .toString()
         ));
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(out.toString(), is(
                 new StringBuilder()
-                        .append("    o    ").append(lineSeparator)
-                        .append("   o o   ").append(lineSeparator)
-                        .append("  o   o  ").append(lineSeparator)
-                        .append(" o     o ").append(lineSeparator)
-                        .append("o o o o o").append(lineSeparator).append(lineSeparator)
-                        .toString()
+                .append("    o    ").append(lineSeparator)
+                .append("   o o   ").append(lineSeparator)
+                .append("  o   o  ").append(lineSeparator)
+                .append(" o     o ").append(lineSeparator)
+                .append("o o o o o").append(lineSeparator).append(lineSeparator)
+                .toString()
         ));
-        System.setOut(stdout);
     }
 }
