f6a6702097b520ed9c76750a240893cf7b255d6b
ahorn80 <ahorn80@gmx.de>
2018-05-01 14:17:29 +0200

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,38 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+
+    final java.lang.String lineSeparator = System.getProperty("line.separator");
+    /**
+     * default output to console
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * buffer for output
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();
@@ -55,5 +83,85 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"0", "name1", "desc1", "0", "name2", "desc2", "1", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.findAll().length, is(2));
+    }
+
+    @Test
+    public void whenShowAllItemsThenShowThem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(this.getMenuText())
+                                .append("------------ show all requests --------------").append(lineSeparator)
+                                .append("ID: ").append(item.getId())
+                                .append(" name: ").append(item.getName())
+                                .append(" description: ").append(item.getDesc()).append(lineSeparator)
+                                .append(this.getMenuText())
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenShowItemByIDThenShowIt() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(this.getMenuText())
+                                .append("------------ find request by id--------------").append(lineSeparator)
+                                .append("ID: ").append(item.getId())
+                                .append(" name: ").append(item.getName())
+                                .append(" description: ").append(item.getDesc()).append(lineSeparator)
+                                .append(this.getMenuText())
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenShowItemByNameThenShowIt() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "desc"));
+        Input input = new StubInput(new String[]{"5", "name", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(
+                out.toString(),
+                is(
+                        new StringBuilder()
+                                .append(this.getMenuText())
+                                .append("------------ find requests by name--------------").append(lineSeparator)
+                                .append("ID: ").append(item.getId())
+                                .append(" name: ").append(item.getName())
+                                .append(" description: ").append(item.getDesc()).append(lineSeparator)
+                                .append(this.getMenuText())
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * get menu text from StartUI
+     * @return
+     */
+    private String getMenuText() {
+        return  new StringBuilder()
+                .append("Menu:").append(lineSeparator)
+                .append("0. Add new Item").append(lineSeparator)
+                .append("1. Show all items").append(lineSeparator)
+                .append("2. Edit item").append(lineSeparator)
+                .append("3. Delete item").append(lineSeparator)
+                .append("4. Find item by Id").append(lineSeparator)
+                .append("5. Find items by name").append(lineSeparator)
+                .append("6. Exit Program").append(lineSeparator)
+                .toString();
     }
 }
